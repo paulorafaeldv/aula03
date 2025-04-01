6a584b7f7888ae5dab06f877974571556f8e8cf9
paulo <paulorafaelbrandao@gmail.com>
2025-04-01 15:56:03 -0300

    atividade 5

---
+++ atividade4.py
@@ -0,0 +1,6 @@
+a = int(input("digite o primeiro número: "))
+b = int(input("digite o segundo número: "))
+if a < b:
+    print (a, b)
+else:
+    print (b, a)
---
+++ atividade5.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Informe a primeira nota: "))
+nota2 = float(input("Informe a segunda nota: "))
+nota3 = float(input("Informe a terceira nota: "))
+m = (nota1 + nota2 + nota3)/3
+if m >= 7:
+    print (f"Sua média foi {m:.2f}. \033[32mParabens! Aprovado\033[m")
+else:
+    print (f"Sua média foi {m:.2f}. \033[31mReprovado\033[m")
