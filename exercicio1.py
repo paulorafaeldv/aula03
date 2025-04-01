5f5e675bca95b4ffd51a7e2455e7ba51823e0994
paulo <paulorafaelbrandao@gmail.com>
2025-04-01 14:15:30 -0300

    adicionei aumento percentual no salario

--- exercicio1.py
+++ exercicio1.py
@@ -1,4 +1,9 @@
 n = input("Informe seu nome: ")
 i = int(input('informe sua idade: '))
 s = float(input("informe seu salário: "))
-print (f"Seu nome é {n}, você tem {i} anos e seu salário é \033[31mR${s:.2f} reais")
+p = float(input("Informe o percentual de aumento em porcentagem: "))
+sa = s + s * (p/100)
+print (f"Seu nome é {n}, você tem {i} anos e seu salário é \033[1;31;42mR${s:.2f}\033[m reais")
+print (f"Com o aumento de {p}%, o salário será {sa}")
+
+# pedir percentual de aumento, calcular o percentual e pedir o valor com o p
