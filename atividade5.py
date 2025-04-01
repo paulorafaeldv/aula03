814cf2b44a9733a2cb2305931af2d7e40fb78c24
paulo <paulorafaelbrandao@gmail.com>
2025-04-01 16:18:45 -0300

    atividade5

--- atividade5.py
+++ atividade5.py
@@ -1,3 +1,4 @@
+
 nota1 = float(input("Informe a primeira nota: "))
 nota2 = float(input("Informe a segunda nota: "))
 nota3 = float(input("Informe a terceira nota: "))
@@ -5,4 +6,7 @@ m = (nota1 + nota2 + nota3)/3
 if m >= 7:
     print (f"Sua média foi {m:.2f}. \033[32mParabens! Aprovado\033[m")
 else:
-    print (f"Sua média foi {m:.2f}. \033[31mReprovado\033[m")
+    if m < 4:
+        print(f"Sua média foi {m:.2f}. \033[31mReprovado\033[m")
+    else:
+        print(f"Sua média foi {m:.2f}. \033[33mEstá de recuperação\033[m")
