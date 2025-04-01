fe19f6703e5b1600462c22de3d31eab7e0194d95
paulo <paulorafaelbrandao@gmail.com>
2025-04-01 13:59:24 -0300

    commit do exercicio 1 da aula 3

---
+++ exercicio1.py
@@ -0,0 +1,4 @@
+n = input("Informe seu nome: ")
+i = int(input('informe sua idade: '))
+s = float(input("informe seu salário: "))
+print (f"Seu nome é {n}, você tem {i} anos e seu salário é \033[31mR${s:.2f} reais")
