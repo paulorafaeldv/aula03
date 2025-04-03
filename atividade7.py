90d4c76124fd3394b38b13ea1b4b7271a694e7de
paulo <paulorafaelbrandao@gmail.com>
2025-04-03 13:57:04 -0300

    Atividade 7

---
+++ atividade7.py
@@ -0,0 +1,12 @@
+c = input("Qual o tipo de combustível? Escreva G para gasolina e E para etanol: ").strip().lower()
+v = float(input("Informe quantos litros: "))
+if c == 'e':
+    r = v * 4.9
+    print (f"O valor será R${r:.2f}")
+elif c == 'g':
+    r = v * 5.8
+    print (f"O valor será R${r:.2f}")
+else:
+    r = v * 0
+    print(f"Resposta inválida. Valor R${r:.2f}")
+
