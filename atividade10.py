13de4ae797613c27c4579e87c783c1d85b455cef
paulo <paulorafaelbrandao@gmail.com>
2025-04-08 16:34:28 -0300

    Atualização atividade for

--- atividade10.py
+++ atividade10.py
@@ -4,4 +4,5 @@ for n in range (10):
     soma = soma + n
 print (f"A soma é {soma}")
 
-# start, stop, step. O único obrigatório é o stop
+# start: onde vai iniciar, stop: onde vai parar, step: de quanto em quanto.
+# O único obrigatório é o stop
